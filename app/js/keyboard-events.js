d489a01a805c249d3fa51bd4abe85138383756ae
Diego Islas Ocampo <hyuchia@gmail.com>
2017-04-26 17:54:50 -0500

    Add edition keyboard events

--- app/js/keyboard-events.js
+++ app/js/keyboard-events.js
@@ -6,6 +6,60 @@ $_ready(function(){
 		}
 	});
 
+	keyboardJS.bind('ctrl + b', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand("bold", false, null);
+		}
+	});
+
+	keyboardJS.bind('ctrl + i', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand("italic", false, null);
+		}
+	});
+
+	keyboardJS.bind('ctrl  + u', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand("underline", false, null);
+		}
+	});
+
+	keyboardJS.bind('ctrl + h + 1', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand('formatBlock', false, '<h1>');
+		}
+	});
+
+	keyboardJS.bind('ctrl + h + 2', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand('formatBlock', false, '<h2>');
+		}
+	});
+
+	keyboardJS.bind('ctrl + h + 3', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand('formatBlock', false, '<h3>');
+		}
+	});
+
+	keyboardJS.bind('ctrl + h + 4', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand('formatBlock', false, '<h4>');
+		}
+	});
+
+	keyboardJS.bind('ctrl + h + 5', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand('formatBlock', false, '<h5>');
+		}
+	});
+
+	keyboardJS.bind('ctrl + h + 6', function () {
+		if ($_("#editor").isVisible()) {
+			document.execCommand('formatBlock', false, '<h6>');
+		}
+	});
+
 	keyboardJS.bind('ctrl + n', function () {
 		if ($_("[data-view='notes']").isVisible()) {
 			wait("Wait while your note is created");
