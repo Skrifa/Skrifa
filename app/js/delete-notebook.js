0c9cf7f98c7f7390bad7a7acd8bcd803d2814759
Diego Islas Ocampo <hyuchia@gmail.com>
2017-05-28 20:21:10 -0500

    Change behavior to match new buttons

--- app/js/delete-notebook.js
+++ app/js/delete-notebook.js
@@ -3,29 +3,30 @@ $_ready(() => {
 	// Listener for the submit button
 	$_("[data-form='delete-notebook']").submit(function(event) {
 		event.preventDefault();
-		var self = this;
+	});
+
+	$_("[data-form='delete-notebook'] [data-action='delete-move-inbox']").click(function(event) {
 		wait("Deleting Your Notebook");
 		db.transaction('rw', db.notebook, db.note, function() {
-			// Delete Notebook
 			db.notebook.where("id").equals(parseInt(notebook)).delete();
-
-			// Check what strategy was chosen
-			switch ($_("[data-form='delete-notebook'] [name='strategy']:checked").value()) {
-				// Move all notes that pointed to the deleted notebook to Inbox
-				case "move":
-					db.note.where("Notebook").equals(notebook).modify({Notebook: "Inbox"});
-					break;
-
-				// Delete all notes that pointed to the deleted notebook
-				case "delete":
-					db.note.where("Notebook").equals(notebook).delete();
-					break;
-			}
-
+			db.note.where("Notebook").equals(notebook).modify({Notebook: "Inbox"});
 		}).then(function(){
-			// Clear form
-			self.reset();
+			// Change view to the Inbox notebook
+			notebook = "Inbox";
+			$_(".logo h1").text("Inbox");
+			$_(".logo small").text("A place for any note");
+			$_("[data-action='edit-notebook']").hide();
+			$_("[data-action='delete-notebook']").hide();
+			loadContent();
+		});
+	});
 
+	$_("[data-form='delete-notebook'] [data-action='delete-everything']").click(function(event) {
+		wait("Deleting Your Notebook");
+		db.transaction('rw', db.notebook, db.note, function() {
+			db.notebook.where("id").equals(parseInt(notebook)).delete();
+			db.note.where("Notebook").equals(notebook).delete();
+		}).then(function(){
 			// Change view to the Inbox notebook
 			notebook = "Inbox";
 			$_(".logo h1").text("Inbox");
